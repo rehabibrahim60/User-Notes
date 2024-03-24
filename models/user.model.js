931a9d70ddb2a36cda224fb86cb9ba1e9cc891fd
rehabibrahim60 <132395261+rehabibrahim60@users.noreply.github.com>
2024-03-25 01:02:25 +0200

    Delete models directory

--- models/user.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const userSchema=new Schema({
-    name:String,
-    email:String,
-    password:String
-},{timestamps:true})
-
-export const userModel = mongoose.model('user',userSchema)
